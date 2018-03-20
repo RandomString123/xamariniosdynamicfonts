492e04b4f5f5d9fd17648b4171c4517efa2138a6
Matthew Churilla <matthew.churilla@outlook.com>
2018-03-19 20:17:55 -0400

    Initial Project Creation

---
+++ XamarinIOSDynamicFonts/XamarinIOSDynamicFonts/XamarinIOSDynamicFonts.iOS/DynamicFontLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XamarinIOSDynamicFonts;
+
+using Foundation;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(XamarinIOSDynamicFonts.iOS.IOSFontLoader))]
+namespace XamarinIOSDynamicFonts.iOS
+{
+    public class IOSFontLoader : IFontLoader
+    {
+        public void LoadFile(string bundleName)
+        {
+            NSError t;
+            CoreGraphics.CGDataProvider provider = CoreGraphics.CGDataProvider.FromFile(bundleName);
+            if(provider == null)
+            {
+                Console.WriteLine("Provider " + bundleName + " is not valid.");
+                return;
+            }
+            //ProTip: Examine font in a dubugger to see details of the font file.
+            CoreGraphics.CGFont font = CoreGraphics.CGFont.CreateFromProvider(provider);
+            if(!CoreText.CTFontManager.RegisterGraphicsFont(font, out t))
+            {
+                Console.WriteLine("Unable to register font file: " + bundleName);
+            }
+        }
+    }
+}
