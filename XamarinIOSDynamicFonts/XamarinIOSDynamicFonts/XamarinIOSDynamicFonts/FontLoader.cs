918e49d194b6f1812cfe3506489c444619e37f08
Matthew Churilla <matthew.churilla@outlook.com>
2018-03-19 20:19:36 -0400

    Font loader interface.  Defines method to be used with the dependency service.

---
+++ XamarinIOSDynamicFonts/XamarinIOSDynamicFonts/XamarinIOSDynamicFonts/FontLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinIOSDynamicFonts
+{
+    public interface IFontLoader
+    {
+        void LoadFile(string bundleName);
+    }
+}
